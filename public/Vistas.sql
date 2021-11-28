e27e62bd48145d3c42be94ce5614cf0182616602
Oscar Quintero <oscar24quintero@hotmail.com>
2021-11-28 01:32:19 -0600

    Creación de archivo para guardar definición de vistas en la BD

---
+++ public/Vistas.sql
@@ -0,0 +1,15 @@
+
+
+CREATE VIEW
+VistaCultivo AS
+SELECT IDCultivo, Cultivo.IDCampesino, NombreCultivo, Nombre as NombrePropietario, EspecieVegetal.IDEspecieVegetal, NombreEspecie, CaudalDeRiego, VolumenAguaRequeridaAlDia FROM Cultivo 
+	JOIN Campesino
+    ON Cultivo.IDCampesino = Campesino.IDCampesino
+    JOIN EspecieVegetal 
+    ON Cultivo.IDEspecieVegetal = EspecieVegetal.IDEspecieVegetal;
+
+CREATE OR REPLACE VIEW 
+VistaCampesinosConCultivos AS
+	SELECT IDCampesino, NombrePropietario AS NombreCampesino, COUNT(*) AS CantidadDeCultivos 
+	FROM vistacultivo 
+	GROUP BY IDCampesino;
