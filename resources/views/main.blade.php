eae02a77c81e8da37a881f64aa4f761df7ed96bb
Oscar Quintero <oscar24quintero@hotmail.com>
2021-11-28 01:28:39 -0600

    Creación del layout principal en el main

---
+++ resources/views/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>RiegApp</title>
+    @include('links-and-scripts')
+  </head>
+  <body>
+    <div
+      class="flex h-screen bg-gray-50 dark:bg-gray-900"
+      :class="{ 'overflow-hidden': isSideMenuOpen }"
+    >
+      @include('lateral-bar')      
+      {{-- Area principal --}}
+      <div class="flex flex-col flex-1 w-full">
+        @include('header')
+       
+
+        <main class="h-full overflow-y-auto">
+            <h2
+              class="my-6 text-4xl font-semibold text-gray-700 dark:text-gray-200"
+            >
+              @yield('section-title')
+            </h2>
+          @yield('content')
+        </main>
+      </div>
+    </div>
+  </body>
+</html>
