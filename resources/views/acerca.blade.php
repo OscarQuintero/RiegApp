de52c9857185e0e9eb406648a1bbd8e2f6def7e7
Oscar Quintero <oscar24quintero@hotmail.com>
2021-11-28 20:18:46 -0600

    Adición de esqueleto a Acerca

--- resources/views/acerca.blade.php
+++ resources/views/acerca.blade.php
@@ -5,6 +5,39 @@
 @section('content')
 	<h3>Elaborado por Oscar Quintero</h3>
 	<h3>Seminario de SDPD Ingeniería de Software I</h3>
+			<h4
+              class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300"
+            >
+              Cards with title
+            </h4>
+            <div class="grid gap-6 mb-8 md:grid-cols-2">
+              <div
+                class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800"
+              >
+                <h4 class="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+                  Revenue
+                </h4>
+                <p class="text-gray-600 dark:text-gray-400">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Fuga, cum commodi a omnis numquam quod? Totam exercitationem
+                  quos hic ipsam at qui cum numquam, sed amet ratione! Ratione,
+                  nihil dolorum.
+                </p>
+              </div>
+              <div
+                class="min-w-0 p-4 text-white bg-purple-600 rounded-lg shadow-xs"
+              >
+                <h4 class="mb-4 font-semibold">
+                  Colored card
+                </h4>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Fuga, cum commodi a omnis numquam quod? Totam exercitationem
+                  quos hic ipsam at qui cum numquam, sed amet ratione! Ratione,
+                  nihil dolorum.
+                </p>
+              </div>
+            </div>
 
 	
 	
