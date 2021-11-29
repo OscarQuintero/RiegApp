bce9457f7b440316b91bb4589b7a452e4aeb110a
Oscar Quintero <oscar24quintero@hotmail.com>
2021-11-28 20:16:14 -0600

    Creación de Vista para planes de riego

--- public/Vistas.sql
+++ public/Vistas.sql
@@ -3,7 +3,7 @@
 CREATE OR REPLACE VIEW 
 VistaCultivo AS
 SELECT IDCultivo, Cultivo.IDCampesino, NombreCultivo, Nombre as NombrePropietario, Area, EspecieVegetal.IDEspecieVegetal, NombreEspecie, CaudalDeRiego, VolumenAguaRequeridaAlDia FROM Cultivo 
-	JOIN Campesino
+	LEFT JOIN Campesino
     ON Cultivo.IDCampesino = Campesino.IDCampesino
     JOIN EspecieVegetal 
     ON Cultivo.IDEspecieVegetal = EspecieVegetal.IDEspecieVegetal;
@@ -13,3 +13,10 @@ VistaCampesinosConCultivos AS
 	SELECT IDCampesino, NombrePropietario AS NombreCampesino, COUNT(*) AS CantidadDeCultivos 
 	FROM vistacultivo 
 	GROUP BY IDCampesino;
+
+CREATE OR REPLACE VIEW
+VistaPlanesDeRiego AS
+	SELECT IDPlanDeRiego, P.IDCultivo, C.NombrePropietario ,C.NombreCultivo, NombreEspecie, Fecha, HoraInicio, HoraFin, CaudalDeRiego, PorcentajeAgua
+    FROM PlanDeRiego AS P
+    JOIN VistaCultivo AS C
+    ON P.IDCultivo = C.IDCultivo;
